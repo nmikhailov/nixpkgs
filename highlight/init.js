9b4b8e478e6b5b25dfb29f18f82ba517cca1e76d
Nikita Mikhailov <nslqqq@gmail.com>
2015-03-28 21:18:01 +0100

    rm

--- highlight/init.js
+++
@@ -1,7 +0,0 @@
-document.onreadystatechange = function () {
-  var listings = document.querySelectorAll('.programlisting, .screen');
-
-  for (i = 0; i < listings.length; ++i) {
-    hljs.highlightBlock(listings[i]);
-  }
-}
